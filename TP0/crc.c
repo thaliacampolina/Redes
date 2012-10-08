65991789524e4bd15129268afa43d7d2959829b5
Thalia <thalia@thatha.(none)>
2012-10-08 00:42:55 -0300

    is reading the file, needs the crc implementation

--- TP0/crc.c
+++ TP0/crc.c
@@ -13,7 +13,7 @@ void ReadFile(FILE* input, int polinomio){
     char d;
     //char* e= (char*)malloc(sizeof (char));
     int i;
-    int mod;
+    float mod;
 printf("conteudo arquivo:  ");
     while ((fscanf(input, "%c", &c) > 0)){
         if (polinomio == 1) {
@@ -24,10 +24,11 @@ printf("%c = ",c);
         i = (int) c;
 printf("%i   ",i);
         mod = Mod(i,polinomio);
+printf("mod dentro do read = %f \n", mod);
    //     Hexadecimal(mod);
    }
-    fclose (input);
     //free(e);
+printf("\n ta fechando o arquivo! \n");
     printf("\n");
 
 }
@@ -36,16 +37,18 @@ int Mod8(int i){
     //8-bit polynomial = x⁸ + x² + x + 1
     //binary value: 100000111
     //decimal: 263
-    int mod = i%263;
+    float mod = i%263;
     return mod;
+printf("mod de 8 bits = %f \n", mod);
 }
 
 int Mod16(int i){
     //16-bit polynomial =x¹⁶+X¹⁵+x²+1 
     //binary value: 11000000000000101
     //decimal: 98309
-    int mod = i%98309;
+    float mod = i%98309;
     return mod;
+printf("mod de 16 bits = %f \n", mod);
 }
 
 int Mod(int i, int polinomio){
