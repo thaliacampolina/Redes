f18a5d8de8ff90c53d7a8d6971b924fe1e2e0cd6
Thalia <thalia@thatha.(none)>
2012-10-14 14:38:48 -0300

    unigned int. THE END <3

--- TP0/crc.c
+++ TP0/crc.c
@@ -11,16 +11,16 @@
 
 void ReadFile(FILE* input, int polinomio){
     char first, second, str[2];
-    int f, s, sixteen;
+    unsigned int f, s, sixteen;
     printf("0x");
     while ((fscanf(input, "%c", &first) > 0)){
-	f = (int) first;
+	f = (unsigned int) first;
         if (polinomio == 1) {
 	//if 16-bit polynomial, two strings are concatenated
             fscanf(input, "%c", &second);
             str[0]=first;
 	    str[1]=second;
-	    s = (int) second;
+	    s = (unsigned int) second;
 	    sixteen = f << 8;
 	    sixteen = sixteen|s;
         }
@@ -30,17 +30,17 @@ void ReadFile(FILE* input, int polinomio){
    printf("\n\n");
 }
 
-int shift8(int i){
+int shift8(unsigned int i){
 	i = i<<8;
 	return i;
 	}
 	
-long int shift16(int i){
+long int shift16(unsigned int i){
 	long int a = i<<16;
 	return a;
 	}
 	
-void printHex(int mod){
+void printHex(unsigned int mod){
     char s[256];
     int k=0;
     sprintf(s, "%x", mod);
@@ -50,25 +50,25 @@ void printHex(int mod){
     }
 }
 
-void Mod8(int i){
+void Mod8(unsigned int i){
     //8-bit polynomial = x⁸ + x² + x + 1
     //binary value: 100000111
     //decimal: 263
     i = shift8(i);
-    int mod = i%263;
+    unsigned int mod = i%263;
     printHex(mod);
 }
 
-void Mod16(int i){
+void Mod16(unsigned int i){
     //16-bit polynomial =x¹⁶+X¹⁵+x²+1 
     //binary value: 11000000000000101
     //decimal: 98309
     long int a = shift16(i);
-    int mod = a%98309;
+    unsigned int mod = a%98309;
     printHex(mod);
 }
 
-void Mod(int i, int polinomio){
+void Mod(unsigned int i, int polinomio){
     if (polinomio == 0) Mod8(i);
     else Mod16(i);
 }
--- TP0/crc.h
+++ TP0/crc.h
@@ -4,20 +4,17 @@
 
 void ReadFile(FILE* input, int polinomio);
 //calculates mod for 8-bit polynomial
-int shift8(int i);
+int shift8(unsigned int i);
 //shifts 8 bits to the left
-long int shift16(int i);
+long int shift16(unsigned int i);
 //shitfs 16 bits to the left
-void printHex(int mod);
+void printHex(unsigned int mod);
 //prints the remainder of the polynomial division in hexadecimal format
-void Mod8(int i);
+void Mod8(unsigned int i);
 //calculates mod for 16-bit polynomial
-void Mod16(int i);
+void Mod16(unsigned int i);
 //calculates mod
-void Mod(int i, int polinomio);
-//prints a hexadecimal value on screen*********************EDIT
-void Hexadecimal(int mod);
-
+void Mod(unsigned int i, int polinomio);
 
 
 #endif
