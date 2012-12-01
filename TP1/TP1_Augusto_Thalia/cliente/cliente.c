48c589b39be50db2f53e0faa5352c963b1592589
Thalia <thaliacampolina@hotmail.com>
2012-12-01 17:38:36 -0200

    addind client

---
+++ TP1/TP1_Augusto_Thalia/cliente/cliente.c
@@ -0,0 +1,62 @@
+/*IMPLEMENTACAO DO CLIENTE*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <limits.h>
+// Include BASE64
+#include "encode.h"
+
+int main(int argc, char* argv[]) {
+/*
+/////////////////////////////////////////////////////////////
+//Pega dados usuário:
+
+    	char host[16] ;			//ip servidor
+	char* username = "";		//username
+	char* password = "";
+	
+	if (argc <> 4)
+		{
+		printf("ERRO: Argumentos errados\nReinicie com os argumentos corretos: <username> <password> <IP do servidor>");
+		return 0;
+		}
+
+	// armazena argumentos
+	username = argv[1];
+	password = argv[2];
+	host = argv[3];
+*/
+//////////////////////////////////////////////////////////////////
+// Variaveis SOCKET
+
+    int sock; //inteiro que recebe o socket
+
+	string envia, recebe; //strings de envio e recebimento de msgs
+
+	char char_recebe[200]; //usado como parametro no recebimento
+
+    const char *char_envia; //usado como parametro para envio
+
+    struct sockaddr_in server; //estrutura de comunicação
+
+
+///////////////////////////////////////////////////////////////////
+// Inicia conexão servidor
+
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+
+
+//////////////////////////////////////////////////////////////////
+// FECHA CONEXAO
+	close (sock);
+        return 0;
+}
