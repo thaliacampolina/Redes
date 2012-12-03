8f5842c9ffb51a21ba3c0ef3b3fadd95a83ff1cf
Augustto Oliveira <augustto@augustto-laptop.(none)>
2012-12-03 00:39:41 -0200

    cliente e servidor, funcionando user, pass, stat, list, retr implementado não funcionanando

--- TP1/EXAMPLE/client.c
+++ TP1/EXAMPLE/client.c
@@ -5,7 +5,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
 #include <netdb.h> 
+#define PORTA 1234
 
 void error(const char *msg)
 {
@@ -13,45 +15,53 @@ void error(const char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
+int main(int argc, char *argv[]){
+	char host[16];		//ip servidor
+	char username[50];		//username
+	char password[50];
+	if (argc != 4)
+		{
+		printf("ERRO: Argumentos errados\nReinicie com os argumentos corretos: <username> <password> <IP do servidor>");
+		return 0;
+		}
+
+	// armazena argumentos
+	strcpy(username,argv[1]);
+	strcpy(password,argv[2]);
+	strcpy(host,argv[3]);
+    int sockfd, portno, n=0;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
+    char hostname[1024];
+    hostname[1023]='\0';
+    gethostname(hostname, 1023);
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+    if (sockfd < 0) error("ERROR opening socket"); //erro na abertura do socket
+    server = gethostbyname(hostname);
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(PORTA);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting"); //erro na conexão
+    while(1){
+	    bzero(buffer,256);
+	    fgets(buffer, 255, stdin);
+	    n = write(sockfd,buffer,strlen(buffer));
+	    if (n < 0) error("ERROR writing to socket");
+	    if (strncmp(buffer, "QUIT", 4) == 0) break;
+	    else if (strncmp(buffer, "STAT", 4) == 0) printf("type enter again");
+	    else if (strncmp(buffer, "LIST", 4) == 0) printf("type enter again");	    
+	    bzero(buffer,256);
+//	    sleep(10);
+//	    while(n==0){
+	    	n = read(sockfd,buffer,255);
+//	    	sleep(1);
+//	    }	    	
+	    if (n < 0) error("ERROR reading from socket");
+	    printf("%s\n",buffer);
+    }
     close(sockfd);
+    shutdown(sockfd, 2);
     return 0;
 }
--- TP1/EXAMPLE/server.c
+++ TP1/EXAMPLE/server.c
@@ -6,7 +6,12 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
+#define PORTA 1234
+
+//vetor que guarda os tamanhos de cada arquivo (e-mail)
+static vetor_tamanhos[100];
 
 void error(const char *msg)
 {
@@ -14,42 +19,118 @@ void error(const char *msg)
     exit(1);
 }
 
+int conta_emails(char nome[50]){
+	int contador=0;
+	FILE *input;
+	char nova[60], number;
+	strcpy(nova,nome);
+	strcat(nova, ".1.txt");
+	int size=strlen(nova);
+	while(1){
+		input = fopen(nova, "r");
+		if(input==NULL) break;
+		fclose(input);
+		contador++;
+		number = (contador+48);
+		nova[size-5]=number;
+		}
+	contador--;
+	return contador;
+}
+
+void tamanho_emails(char nome[50]/*, int* tamanho[]*/){
+	int contador=1, x[50];
+	FILE *input;
+	char nova[60], number;
+	strcpy(nova,nome);
+	strcat(nova, ".1.txt");
+	int size=strlen(nova);
+	while(1){
+		input = fopen(nova, "r");
+		if(input==NULL) break;
+		fseek (input, 1 , SEEK_END);
+		vetor_tamanhos[contador] = (ftell(input)-1);
+		fclose(input);
+		contador++;
+		number = (contador+48);
+		nova[size-5]=number;
+		}
+}
+
+int tamanho_total(){
+	int i, total=0;
+	for (i=0; i<50; i++){
+		total+= (vetor_tamanhos[i]);
+		}
+	return total;
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+     int sockfd, newsockfd, portno, i, tamanho_nome, j, n, *tamanho;
+     FILE *retrieve;
      socklen_t clilen;
-     char buffer[256];
+     char buffer[256], nome[50], nomedoarquivo[60], caractere, mensagem[100], msg_curta[10], buffer_retr[10*1024];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+     //declaração das variaveis
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+     if (sockfd < 0) error("ERROR opening socket"); //erro na abertura do socket
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     serv_addr.sin_port = htons(PORTA);
+     //conexão
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding"); //erro na vinculação
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); //aceitar a conexão
+     if (newsockfd < 0) error("ERROR on accept"); //erro na aceitação
+     while(1){
+	     bzero(buffer,256);
+	     n = read(newsockfd,buffer,255);
+	     if (n < 0) error("ERROR reading from socket"); //erro na leitura do socket do cliente
+	     switch (buffer[0]){
+		     case 'U':	j=5;
+		     		for (i=0; i<50; i++){
+					buffer[i]=buffer[j];
+					j++;
+					}
+				strcpy(nome, buffer);
+				tamanho_nome=strlen(nome);
+				nome[tamanho_nome-1]='\0';
+				break;
+		     case 'P':  tamanho_emails(nome);
+		     		sprintf(mensagem, "+OK user %s has %i messages (%i bytes)\n", nome, conta_emails(nome), tamanho_total());
+		     		write(newsockfd, mensagem, strlen(mensagem));
+		     		break;
+		     case 'S':  tamanho_emails(nome);
+		     		sprintf(mensagem, "+STATOK %i %i", conta_emails(nome), tamanho_total());
+		     		fflush(stdin);
+		     		write(newsockfd, mensagem, strlen(mensagem));
+		     		break;
+		     case 'L':	tamanho_emails(nome);
+		     		sprintf(mensagem, "+LISTOK %i messages (%i bytes)\n", conta_emails(nome), tamanho_total());
+		     		fflush(stdin);
+		     		write(newsockfd, mensagem, strlen(mensagem));
+		     		for (i=1; i<=(conta_emails(nome)); i++){
+		     			sprintf(msg_curta, "%i %i\n", i, vetor_tamanhos[i]);
+		     			write(newsockfd, msg_curta, strlen(msg_curta));
+		     			}
+		     		break;
+		     case 'R':  strcpy(nomedoarquivo, nome);
+		     		strcat(nomedoarquivo, ".1.txt");
+		     		caractere=buffer[5];
+     				nomedoarquivo[(strlen(nomedoarquivo))-5]=caractere;
+		     		retrieve=fopen(nomedoarquivo, "r");
+		     		while(!EOF) fscanf(retrieve, "%s", &buffer_retr);
+		     		puts(buffer_retr);
+		     case 'Q':	close(newsockfd);
+		     		close(sockfd);
+		     		return 0;
+		     default: 	break;
+		     }
+	     n = write(newsockfd," ",1);
+	     if (n < 0) error("ERROR writing to socket"); //erro na escrita no socket do cliente
+     }
      return 0; 
 }
